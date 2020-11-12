449ed6246026448a86713d9d433f2b17ea5d3ce2
Mario Stehling <mario.stehling@gmx.de>
2020-11-12 13:11:39 +0100

    Cleaned models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,6 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, :cocktail, :ingredient, presence: true
-  # validates [:cocktail_id, :ingredient_id], uniqueness: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
